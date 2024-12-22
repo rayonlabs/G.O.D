50fb16ca3b8c09dc97dc6907747c06bdc3982396
wanderingweights <subiawaud@gmail.com>
2024-12-22 08:29:07 -0600

    merge fix

--- validator/cycle/main.py
+++ validator/cycle/main.py
@@ -5,14 +5,12 @@ load_dotenv(".vali.env")
 
 import asyncio
 
-
 from validator.core.config import load_config
 from validator.core.refresh_nodes import refresh_nodes_periodically
 from validator.core.weight_setting import set_weights_periodically
 from validator.cycle.process_tasks import process_completed_tasks
 from validator.cycle.process_tasks import process_pending_tasks
 from validator.tasks.synthetic_scheduler import schedule_synthetics_periodically
-from validator.utils.call_endpoint import sign_up_cron_job
 from validator.utils.util import try_db_connections
 
 
@@ -26,7 +24,7 @@ async def run_validator_cycles() -> None:
         set_weights_periodically(config),
         process_completed_tasks(config),
         process_pending_tasks(config),
-        sign_up_cron_job(config.keypair),
+    #    sign_up_cron_job(config.keypair),
     )
 
 
