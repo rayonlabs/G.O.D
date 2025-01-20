ca20da6cad26c5e9bf485cfcf2cd96e779d1d78d
namoray <neuvaamor@proton.me>
2025-01-20 12:40:42 +0000

    scores url

--- validator/endpoints/auditing.py
+++ validator/endpoints/auditing.py
@@ -1,21 +1,17 @@
-from datetime import datetime
-from datetime import timedelta
 
 from fastapi import APIRouter
 from fastapi import Depends
-from fiber.chain.chain_utils import query_substrate
+from fastapi import HTTPException
 from loguru import logger  # noqa
 
-from validator.core import constants as cts
 from validator.core.config import Config
 from validator.core.dependencies import get_config
 from validator.core.models import Task
-from validator.core.models import TaskResults
 from validator.core.models import TaskWithHotkeyDetails
+from validator.db.sql.auditing import get_latest_scores_url
 from validator.db.sql.auditing import get_recent_tasks
 from validator.db.sql.auditing import get_recent_tasks_for_hotkey
 from validator.db.sql.auditing import get_task_with_hotkey_details
-from validator.db.sql.submissions_and_scoring import get_aggregate_scores_since
 
 
 router = APIRouter(tags=["auditing"])
@@ -39,33 +35,15 @@ async def audit_task_details_endpoint(task_id: str, config: Config = Depends(get
     return await get_task_with_hotkey_details(task_id, config)
 
 
-@router.get("/auditing/scores")
-async def get_scores_for_setting_weights(config: Config = Depends(get_config)) -> list[TaskResults]:
+@router.get("/auditing/scores-url")
+async def get_latest_scores_url_endpoint(config: Config = Depends(get_config)) -> str:
     """
-    Get the scores I had when I last set weights, to prove I did it right
+    Get the scores url for when I last set weights, to prove I did it right
     """
-    substrate, uid = query_substrate(
-        config.substrate,
-        "SubtensorModule",
-        "Uids",
-        [config.netuid, config.keypair.ss58_address],
-        return_value=True,
-    )
-    config.substrate = substrate
-    substrate, current_block = query_substrate(substrate, "System", "Number", [], return_value=True)
-    substrate, last_updated_value = query_substrate(
-        substrate, "SubtensorModule", "LastUpdate", [config.netuid], return_value=False
-    )
-    updated: int = current_block - last_updated_value[uid].value
-
-    seconds_since_update = updated * 12  # 12 is block time in seconds
-
-    time_when_last_set_weights = datetime.now() - timedelta(seconds=seconds_since_update, days=cts.SCORING_WINDOW)
-
-    logger.info(f"Getting scores for time {time_when_last_set_weights.strftime('%Y-%m-%d %H:%M:%S')}")
-
-    scores = await get_aggregate_scores_since(start_time=time_when_last_set_weights, psql_db=config.psql_db)
-    return scores
+    url = await get_latest_scores_url(config)
+    if url is None:
+        raise HTTPException(status_code=400, detail="No scores url found... sorry :/")
+    return url
 
 
 def factory_router():
