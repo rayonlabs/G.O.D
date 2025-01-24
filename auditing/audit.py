ebb4415149dbe84a14492e2765ac5d83c7278f3d
namoray <neuvaamor@proton.me>
2025-01-24 14:15:49 +0000

    temp

--- auditing/audit.py
+++ auditing/audit.py
@@ -151,7 +151,7 @@ async def main():
             f"My Validator Node ID: {uid}. Last updated {updated} blocks ago. Weights set rate limit: {weights_set_rate_limit}."
         )
 
-        if updated < weights_set_rate_limit:
+        if updated < weights_set_rate_limit or True:
             sleep_duration = (weights_set_rate_limit - updated) * 12
             logger.info(f"Sleeping for {sleep_duration} seconds [{sleep_duration / 12 } blocks]" " as we set weights recently...")
             await asyncio.sleep(sleep_duration)
