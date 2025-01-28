ccf2d0f29f69d85d621d962bb454e930cc62c321
Hamza Khan <diagonalge@gmail.com>
2025-01-28 19:09:02 +0500

    small oopsie

--- validator/cycle/main.py
+++ validator/cycle/main.py
@@ -9,6 +9,7 @@ from validator.core.config import load_config
 from validator.core.refresh_nodes import refresh_nodes_periodically
 from validator.cycle.process_tasks import process_completed_tasks
 from validator.cycle.process_tasks import process_pending_tasks
+from validator.tasks.synthetic_scheduler import schedule_synthetics_periodically
 from validator.utils.util import try_db_connections
 
 
@@ -18,7 +19,7 @@ async def run_validator_cycles() -> None:
 
     await asyncio.gather(
         refresh_nodes_periodically(config),
-        # schedule_synthetics_periodically(config),
+        schedule_synthetics_periodically(config),
         process_completed_tasks(config),
         process_pending_tasks(config),
         #    sign_up_cron_job(config.keypair),
