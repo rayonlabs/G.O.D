f4088739a78c4ea10f8fd505393dc76c106ae040
besimray <besimray24@gmail.com>
2025-02-25 21:50:38 +0000

    fix failed evals showing as successful

--- validator/evaluation/scoring.py
+++ validator/evaluation/scoring.py
@@ -755,10 +755,15 @@ async def evaluate_and_score(task: TextRawTask | ImageRawTask, gpu_ids: list[int
     task_results = adjust_miner_scores_to_be_relative_to_other_comps(task_results)
     await _update_scores(task, task_results, config.psql_db)
     all_scores_zero = all(result.score == 0.0 for result in task_results)
-    if all_scores_zero and task.n_eval_attempts < cts.MAX_EVAL_ATTEMPTS - 1:
-        task.status = TaskStatus.PREEVALUATION
-        add_context_tag("status", task.status.value)
-        logger.info(f"All scores are zero for task {task.task_id}, setting status to PREEVALUATION to re-evaluate")
+    if all_scores_zero:
+        if task.n_eval_attempts < cts.MAX_EVAL_ATTEMPTS - 1:
+            task.status = TaskStatus.PREEVALUATION
+            add_context_tag("status", task.status.value)
+            logger.info(f"All scores are zero for task {task.task_id}, setting status to PREEVALUATION to re-evaluate")
+        else:
+            task.status = TaskStatus.FAILURE
+            add_context_tag("status", task.status.value)
+            logger.info(f"Task {task.task_id} failed evaluation after {cts.MAX_EVAL_ATTEMPTS-1} attempts.")
     else:
         if cts.DELETE_S3_AFTER_COMPLETE:
             if task.task_type == TaskType.TEXTTASK:
