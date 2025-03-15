9641454e9ed6726d1f8b15bddef0c1f7c80aa242
WanderingWeights <wanderingweights1@gmail.com>
2025-03-15 10:00:47 -0600

    missed off 100k

--- validator/core/constants.py
+++ validator/core/constants.py
@@ -87,7 +87,7 @@ TEXT_DATASET_BINS_TO_TRAINING_HOURS_RANGE = {
     #   (5_000, 10_000): (3, 5),  # 5k-10k rows needs 1-2 hours
     (10_000, 25_000): (3, 6),  # 10k-25k rows needs 2-4 hours
     (25_000, 50_000): (4, 8),  # 25k-50k rows needs 3-6 hours
-    (50_000, 500_000): (5, 9),  # 50k-500k rows needs 4-8 hours
+    (50_000, 100_000): (5, 9),  # 50k-500k rows needs 4-8 hours
     (100_000, 500_000): (7, 10),  # 50k-500k rows needs 4-8 hours
 }
 
