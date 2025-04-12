648467b89fd217e8dfc44959940915f6e3650885
WanderingWeights <wanderingweights1@gmail.com>
2025-04-12 10:36:27 -0600

    removing being dommi

--- validator/db/migrations/20250410150115_image_model_type.sql
+++ validator/db/migrations/20250410150115_image_model_type.sql
@@ -1,30 +1,18 @@
 -- migrate:up
-BEGIN;
-
 DO $$ BEGIN
     IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'model_type_enum') THEN
         CREATE TYPE model_type_enum AS ENUM ('sdxl', 'flux');
     END IF;
 END $$;
-
 ALTER TABLE image_tasks
 ADD COLUMN model_type model_type_enum DEFAULT 'sdxl';
-
 UPDATE image_tasks
 SET model_type = 'sdxl'
 WHERE model_type IS NULL;
-
 ALTER TABLE image_tasks
 ALTER COLUMN model_type SET NOT NULL;
 
-COMMIT;
-
 -- migrate:down
-BEGIN;
-
 ALTER TABLE image_tasks
 DROP COLUMN IF EXISTS model_type;
-
 DROP TYPE IF EXISTS model_type_enum;
-
-COMMIT;
