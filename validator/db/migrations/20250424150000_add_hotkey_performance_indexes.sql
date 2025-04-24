4f10ab10149ef24f4dc53555e4fa0902e063220e
iambooksmart <iambooksmart@proton.me>
2025-04-24 16:32:12 -0600

    adding index

--- validator/db/migrations/20250424150000_add_hotkey_performance_indexes.sql
+++ validator/db/migrations/20250424150000_add_hotkey_performance_indexes.sql
@@ -2,28 +2,28 @@
 BEGIN;
 
 CREATE INDEX IF NOT EXISTS idx_submissions_hotkey
-ON submissions_table(hotkey);
+ON submissions(hotkey);
 
 CREATE INDEX IF NOT EXISTS idx_submissions_hotkey_created
-ON submissions_table(hotkey, created_on DESC);
+ON submissions(hotkey, created_on DESC);
 
 CREATE INDEX IF NOT EXISTS idx_task_nodes_taskid_hotkey
-ON task_nodes_table(task_id, hotkey);
+ON task_nodes(task_id, hotkey);
 
 CREATE INDEX IF NOT EXISTS idx_submissions_taskid_hotkey
-ON submissions_table(task_id, hotkey);
+ON submissions(task_id, hotkey);
 
 CREATE INDEX IF NOT EXISTS idx_offer_responses_taskid_hotkey
-ON offer_responses_table(task_id, hotkey);
+ON offer_responses(task_id, hotkey);
 
 CREATE INDEX IF NOT EXISTS idx_instruct_text_tasks_taskid
-ON instruct_text_tasks_table(task_id);
+ON instruct_text_tasks(task_id);
 
 CREATE INDEX IF NOT EXISTS idx_image_tasks_taskid
-ON image_tasks_table(task_id);
+ON image_tasks(task_id);
 
 CREATE INDEX IF NOT EXISTS idx_dpo_tasks_taskid
-ON dpo_tasks_table(task_id);
+ON dpo_tasks(task_id);
 
 COMMIT;
 
