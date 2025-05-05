df5f84eb04e47375cec4d2b193604c1258b32860
wanderweights <subiawaud@gmail.com>
2025-05-04 21:06:27 -0600

    newer dataset

--- examples/run_image_task.sh
+++ examples/run_image_task.sh
@@ -2,10 +2,10 @@
 
 TASK_ID="9a877904-5fe9-402a-8c75-be5eb1b51f7e"
 MODEL="zenless-lab/sdxl-anima-pencil-xl-v5"
-DATASET_ZIP="https://gradients.s3.eu-north-1.amazonaws.com/889c0bada81315f0_train_data.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAVVZOOA7SA4UOFLPI%2F20250303%2Feu-north-1%2Fs3%2Faws4_request&X-Amz-Date=20250303T123641Z&X-Amz-Expires=604800&X-Amz-SignedHeaders=host&X-Amz-Signature=178cf09ca66f745a51ade601af84a079051778f1b46a8ad60128ca1cd47a739d"
+DATASET_ZIP="https://gradients.s3.eu-north-1.amazonaws.com/18c12259dda47eac_train_data.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAVVZOOA7SA4UOFLPI%2F20250504%2Feu-north-1%2Fs3%2Faws4_request&X-Amz-Date=20250504T233035Z&X-Amz-Expires=604800&X-Amz-SignedHeaders=host&X-Amz-Signature=c4f230d56569c5ccc3e02a723368b262f76b30cb2e953dd3f95b97840dc735bb"
 MODEL_TYPE="sdxl"
 HOURS_TO_COMPLETE=2
-# Set to a real token if you want to upload the model, or leave as-is to disable upload
+
 HUGGINGFACE_TOKEN="disabled"
 WANDB_TOKEN="your_wandb_token_here"
 HUGGINGFACE_USERNAME="your_hf_username_here"
