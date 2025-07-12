3742a3f2c3abf6a4eb1839ff15409e10bc0d7406
wanderweights <subiawaud@gmail.com>
2025-07-13 07:40:41 +0800

    lg

--- validator/utils/util.py
+++ validator/utils/util.py
@@ -184,30 +184,6 @@ def hide_sensitive_data_till_finished(task: AnyTypeTask) -> AnyTypeTask:
     return task
 
 
-def should_retry_model_loading_on_exception(e):
-    ephemeral_error_patterns = [
-        "does not appear to have a file named",
-        "does not appear to have files named",
-        "Too Many Requests for url",
-    ]
-
-    while e is not None:
-        if isinstance(e, HTTPError):
-            if e.response is None:
-                logger.error(f"HTTPError with no response: {e}, cause: {e.__cause__}")
-                return True
-            elif 500 <= e.response.status_code < 600:
-                return True
-        if any(pattern in str(e) for pattern in ephemeral_error_patterns):
-            return True
-        e = e.__cause__
-    return False
-
-
-def retry_on_5xx():
-    return retry(
-        stop=stop_after_attempt(5),
-        wait=wait_exponential(multiplier=2.5, min=30, max=600),
-        retry=retry_if_exception(should_retry_model_loading_on_exception),
-        reraise=True,
-    )
+# These functions have been moved to retry_utils.py
+from validator.utils.retry_utils import retry_on_5xx
+from validator.utils.retry_utils import should_retry_model_loading_on_exception
