e811298952be1bbfc10b1baf2533579d60f567b6
wanderweights <subiawaud@gmail.com>
2025-07-26 03:46:19 +0700

    only non tourn

--- validator/tasks/synthetic_scheduler.py
+++ validator/tasks/synthetic_scheduler.py
@@ -557,9 +557,9 @@ async def _add_new_task_to_network_if_not_enough(
     dpo_datasets: AsyncGenerator[Dataset, None],
     image_models: AsyncGenerator[ImageModelInfo, None],
 ):
-    current_training_tasks = await get_tasks_with_status(TaskStatus.TRAINING, config.psql_db)
-    current_preeval_tasks = await get_tasks_with_status(TaskStatus.PREEVALUATION, config.psql_db)
-    current_delayed_tasks = await get_tasks_with_status(TaskStatus.DELAYED, config.psql_db, include_not_ready_tasks=True)
+    current_training_tasks = await get_tasks_with_status(TaskStatus.TRAINING, config.psql_db, tournament_filter="exclude")
+    current_preeval_tasks = await get_tasks_with_status(TaskStatus.PREEVALUATION, config.psql_db, tournament_filter="exclude")
+    current_delayed_tasks = await get_tasks_with_status(TaskStatus.DELAYED, config.psql_db, include_not_ready_tasks=True, tournament_filter="exclude")
     total_active_tasks = len(current_training_tasks) + len(current_preeval_tasks)
 
     logger.info(
