2d9f5d0e3ed46c7059b587a34f87eba359e770fb
wanderweights <subiawaud@gmail.com>
2025-07-31 09:44:08 +0700

    lfg

--- examples/run_grpo_task.sh
+++ examples/run_grpo_task.sh
@@ -2,9 +2,9 @@
 
 TASK_ID="8832a4bc-5e99-4d70-a5c9-2605c3b4e1f2"
 MODEL="Qwen/Qwen3-0.6B"
-DATASET="https://gradients.s3.eu-north-1.amazonaws.com/68742916c5fba7e2_test_data.json?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAVVZOOA7SA4UOFLPI%2F20250718%2Feu-north-1%2Fs3%2Faws4_request&X-Amz-Date=20250718T123411Z&X-Amz-Expires=604800&X-Amz-SignedHeaders=host&X-Amz-Signature=7cf19bb88267a6922afae0c845964412c45b63abe6b79983dd1ed10494453190"
+DATASET="https://gradients.s3.eu-north-1.amazonaws.com/ff48d393207a45b9_test_data.json?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAVVZOOA7SA4UOFLPI%2F20250730%2Feu-north-1%2Fs3%2Faws4_request&X-Amz-Date=20250730T195033Z&X-Amz-Expires=604800&X-Amz-SignedHeaders=host&X-Amz-Signature=5318790ec1960c516985bca92847bd5a2871f680034153a3d831fc7d0950a8d8"
 DATASET_TYPE='{
-  "field_prompt":"instruct",
+  "field_prompt":"prompt",
   "reward_functions":[
     {
       "reward_func":"def reward_func(completions, **kwargs):\n    # Count frequency of letter \"e\" in response\n    return [text.count(\"e\") / (len(text) + 1) for text in completions]",
