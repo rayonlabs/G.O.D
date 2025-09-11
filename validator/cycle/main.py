ced0692e47482c16f31d8be0cdd7a0e9f3c73048
diagonalge <diagonalge@gmail.com>
2025-09-11 15:53:22 +0000

    add pending tasks to active

--- validator/cycle/main.py
+++ validator/cycle/main.py
@@ -21,10 +21,10 @@ async def run_validator_cycles() -> None:
     cleanup_temp_files()
 
     await asyncio.gather(
+        process_pending_tasks(config),
         refresh_nodes_periodically(config),
         schedule_synthetics_periodically(config),
         process_completed_tasks(config),
-        process_pending_tasks(config),
         #    sign_up_cron_job(config.keypair),
     )
 
